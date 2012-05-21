d7e084eb891399be4521c55c6156790f64a32247
U-MarieAntoinette\Erin Turnbull <Erin Turnbull@MarieAntoinette.(none)>
2012-05-21 18:01:57 -0400

    This is my first commit ever

---
+++ JoelTest/JoelTest.cpp
@@ -0,0 +1,16 @@
+// JoelTest.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <Windows.h>
+#include <iostream>
+
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	cout<<"fuck yeah";
+	Sleep(1000);
+	return 0;
+}
+
